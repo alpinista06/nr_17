be2f8d9f4c548a5a3978a505744090178ed71b2b
alpnista06 <mariod.b.filho@gmail.com>
2018-08-27 20:04:54 -0300

    atualização de projeto

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Projeto de robo seguidor de linha para o VII TUR , realizado pelo [PET eletrica]
 ![](img/pista7TUR.jpg)
 
 **CURIOSIDADE**  
-O nome deste robô foi inspirado no grande mito, Androide numero 17 vencedor do torneio do poder em dragon ball super.
+O nome deste robô foi inspirado no grande mito, Andróide numero 17 vencedor do torneio do poder em [dragon ball super](https://pt.wikipedia.org/wiki/Dragon_Ball_Super).
 
 ![](img/nr_17.jpg)
 
@@ -34,7 +34,7 @@ Abaixo algumas imagens do primeiro protótipo criado
 
 ## Estratégia Empregada
 
-Para projetar este robô autônomo , em especial para o IIV TUR, listamos os obstáculos a serem vencidos em cada check point para então projetá-lo levando em conta as especificações apresentadas.
+Para projetar este robô autônomo , em especial para o VII TUR, listamos os obstáculos a serem vencidos em cada check point para então projetá-lo levando em conta as especificações apresentadas.
 Foi montado um array de sensores. Para o controle PID desenvolvido, o setpoint é o sensor central e cada um dos laterais gera um certo erro pré-deteminado. Abaixo uma imagem do array.
 
 ![](img/nr17_array_001.jpg)
@@ -49,15 +49,41 @@ Os resultados obtidos com este protótipo foram satisfatórios e podem ser visto
 
 Com isso passamos para a segunda etapa, execução do projeto final.
 
-- [ ] 2° Execução do projeto final
+- [x] 2° Execução do projeto final
 
-Etapa em andamento.
+**Algumas alterações** foram realizadas para buscar um melhor desempenho durante comeptição, entre elas estão a troca dos motores de 1000 rpm para motores de 300 rpm, com essa alteração conseguimos mais estabilidade no controle do robô, adicionamos 2 sensores óptico-reflexivos. O sensor inercial foi retirado, uma vez que com os motores de 300 rpm o torque obtido foi suficiente para vencermos a rampa sem esforço extra.
+Com as mudanças realizadas os resultados se potencializaram, o robô ganhou torque, velocidade e estabilidade. O sistema de controle discreto projetado foi um [controle PD](https://pt.wikipedia.org/wiki/Controlador_proporcional_integral_derivativo) (proporcional-derivativo), o motivo pelo qual foi escolhida essa combinação foi que, em teoria e podendo ser observado na pratica, a ação integral zera o erro de regime permanente como na pista proposta não temos nenhum trecho que possa ser considerado análogo a um regime permanente a ação integral torna-se desnecessária.
 
-:smirk: 
+## Estrutura final do robô
+
+Com as alterações design final ficou da seguinte forma:
+
+Vista frontal
+![](img/frontal.jpg)
+
+Vista lateral
+![](img/lateral.jpg)
+
+Vista superior
+![](img/superior.jpg)
+
+Vista inferior
+![](img/inferior.jpg)
+
+A ideia inicial era de usar uma roda tipo esfera deslizante no eixo frontal, porém como não encontramos a tempo da cometição usando uma porca com ponta de semi-esfera que rendeu resultados satisfatórios. Os slotes de baterias forma dispostos de forma a balancear o centro de massa do carro.
+**OBS:** A estrutura do robô foi refeita,à mão, 1 dia antes do inicio da competição, por esse motivo ele não ficou simétrico como era o esperado.
+
+## Resultados na competição
+
+Durante os testes observamos uma dificuldade em estabilizar o robô, esta dificuldade pode ser atribuída à posição dos sensores, que ficaram demasiadamente próximos ao eixo dos motores exigindo um tempo de resposta maior do sistema de controle. Apesar das dificuldades o melhor desempenho obtido nos rendeu o 4 lugar entre 13 equipes inscritas no torneio.
+
 
 ## license
 
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
 
 ## Autors
 
 Mário Borges - https://github.com/alpinista06
+Guilherme Balduino
+Luiz Henrique (jabora) - https://github.com/rikpalmeiras
--- src/main.ino
+++ src/main.ino
@@ -1,257 +1,698 @@
-///////////
 //Defines//
+
 ///////////
-#define LIMIAR_SENSORES 750
+
+#define LIMIAR_SENSORES 300
+#define CPD 115
+#define CP 15
+
+
 
 //#define TESTE_SENSORES
+
 #define PRINCIPAL
+
 //#define MOSTRAR_VALORES
 
-/////////////////
-//Dados globais//
-////////////////
-//Sensores//
-int sensor_1 = A0;
-int sensor_2 = A1;
-int sensor_3 = A2;
-uint8_t array_sensores = 0;
-int leitura1;
-int leitura2;
-int leitura3;
-//Compensador PID//
-int A = 0.7;
-int Kp = 25;
-int Ki = 35;
-int Kd = 20;
-int resposta_p;
-int resposta_i;
-int resposta_d;
-int Resposta_PID;
-int erro_array;
-int amostra_atual;
-int amostra_anterior;
-int erro_atual;
-int erro_anterior;
-//Velocidade//
-int velocidade;
-int respPI;
-int Vel_up = 255;   // Velocidade de subida
-int Vel_med = 110;   // Velocidade no plano
-int Vel_down = 80;  // Velocidade de descida
-int Vplus, Vless;
-int IN0A, IN1A, IN0B, IN1B;
-int ENA = 9; // motor esquerdo
-int ENB = 10; //motor direito
-//declaracao dos motores//
-int motor1_a = 4;
-int motor1_b = 6;
-int motor2_a = 5;
-int motor2_b = 7;
 
-//Bloco das funções//
+
+/////////////////
+
+//Dados globais//
+
+////////////////
+
+//Sensores//
+int sensor_0 = A0;
+
+int sensor_1 = A1;
+
+
+int sensor_2 = A2;
+
+int sensor_3 = A3;
+
+int sensor_4 = A4;
+
+uint8_t array_sensores = 0;
+
+int leitura0;
+int leitura1;
+
+int leitura2;
+
+int leitura3;
+
+int leitura4;
+
+//Compensador PID//
+
+int Kp = CP;
+
+int Kd = CPD;
+
+int resposta_p = 0;
+
+int resposta_d = 0;
+
+int Resposta_PID = 0;
+
+int erro_array = 0;
+
+int amostra_atual = 0;
+
+int amostra_anterior = 0;
+
+int erro_atual = 0;
+
+int erro_anterior = 0;
+
+//Velocidade//
+
+int velocidade_f;
+int velocidade_1;
+int velocidade_2;
+int velocidade_3;
+int velocidade_4;
+int velocidade_5;
+
+int respPI;
+
+int Vplus, Vless;
+
+int subindo;
+
+int IN0A, IN1A, IN0B, IN1B;
+
+int ENA = 9; // motor esquerdo
+
+int ENB = 10; //motor direito
+
+//declaracao dos motores//
+
+int motor1_a = 4;
+
+int motor1_b = 6;
+
+int motor2_a = 5;
+
+int motor2_b = 7;
+//ligacao do robo//
+int robo_ligado = 0;
+int linha_final;
+
+
+
+//////////////////////////////////////////////////////////////////
+/////////////////Bloco das funções no plano//////////////////////
+/////////////////////////////////////////////////////////////////
 void frente(int velocidade) {
 
+  analogWrite(ENA, velocidade );
 
-  velocidade = 90;
-  analogWrite(ENA, abs(velocidade) );
-  analogWrite(ENB, abs(velocidade) );
+  analogWrite(ENB, velocidade );
 
-  digitalWrite(motor1_a , HIGH);
-  digitalWrite(motor1_b , LOW);
-  digitalWrite(motor2_a , LOW);
-  digitalWrite(motor2_b , HIGH);
-}
 
-//curva compensada à esquerda//
-void curva_a_esquerda(int velocidade, int respPI) {
-
-  Vplus = velocidade + respPI;
-  Vless = velocidade - respPI;
-  analogWrite(ENA, Vplus );
-  analogWrite(ENB, Vless );
 
   digitalWrite(motor1_a , HIGH);
-  digitalWrite(motor1_b , LOW);
-  digitalWrite(motor2_a , LOW);
-  digitalWrite(motor2_b , HIGH);
-}
-//curva compensada à direita//
-void curva_a_direita(int velocidade, int respPI) {
 
-  Vplus = velocidade + respPI;
-  Vless = velocidade - respPI;
-  analogWrite(ENA, Vless );
-  analogWrite(ENB, Vplus );
-
-  digitalWrite(motor1_a , HIGH);
   digitalWrite(motor1_b , LOW);
+
   digitalWrite(motor2_a , LOW);
+
   digitalWrite(motor2_b , HIGH);
+
+  Serial.print("\t");
+  Serial.print(velocidade);
+
 }
 
 //Giro a esquerda desligando um dos motores//
+
 void giro_a_esquerda(int velocidade, int respPI)
 {
-  velocidade = 130;
+
   Vplus = velocidade + respPI;
+  if (Vplus > 255) {
+    Vplus = 255;
+  } else if (Vplus < 0) {
+    Vplus = 0;
+  }
+
   Vless = velocidade - respPI;
+  if (Vless > 255) {
+    Vless = 255;
+  } else if (Vless < 0) {
+    Vless = 0;
+  }
+
   analogWrite(ENA, Vplus );
+
+  analogWrite(ENB, Vless );
+
+
+
+  digitalWrite(motor1_a , HIGH);
+
+  digitalWrite(motor1_b , LOW);
+
+  digitalWrite(motor2_a , LOW);
+
+  digitalWrite(motor2_b , LOW);
+
+  Serial.print("giro a esquerda");
+  Serial.print("\t");
+  Serial.print("motor ENA");
+  Serial.print("\t");
+  Serial.print(Vplus);
+  Serial.print("\t");
+  Serial.print("motor ENB");
+  Serial.print("\t");
+  Serial.print(Vless);
+}
+
+void curva_esquerda(int velocidade, int respPI)
+{
+
+  Vplus = velocidade + respPI;
+  if (Vplus > 255) {
+    Vplus = 255;
+  } else if (Vplus < 0) {
+    Vplus = 0;
+  }
+
+  Vless = velocidade - respPI;
+  if (Vless > 255) {
+    Vless = 255;
+  } else if (Vless < 0) {
+    Vless = 0;
+  }
+
+  analogWrite(ENA, Vplus );
+
   analogWrite(ENB, Vless );
 
   digitalWrite(motor1_a , HIGH);
+
   digitalWrite(motor1_b , LOW);
+
   digitalWrite(motor2_a , LOW);
-  digitalWrite(motor2_b , LOW);
-  // delay(2000);
+
+  digitalWrite(motor2_b , HIGH);
+
+  Serial.print("curva a esquerda");
+  Serial.print("\t");
+  Serial.print("motor ENA");
+  Serial.print("\t");
+  Serial.print(Vplus);
+  Serial.print("\t");
+  Serial.print("motor ENB");
+  Serial.print("\t");
+  Serial.print(Vless);
+
 }
+
 //Giro a direita desligando um dos motores//
+
 void giro_a_direita(int velocidade, int respPI)
+
 {
-  velocidade = 130;
+
   Vplus = velocidade + respPI;
+  if (Vplus > 255) {
+    Vplus = 255;
+  } else if (Vplus < 0) {
+    Vplus = 0;
+  }
+
   Vless = velocidade - respPI;
-  analogWrite(ENA, Vless );
-  analogWrite(ENB, Vplus );
+  if (Vless > 255) {
+    Vless = 255;
+  } else if (Vless < 0) {
+    Vless = 0;
+  }
+
+  analogWrite(ENA, Vplus ); //neste caso e o menor pois o erro e negativo
+
+  analogWrite(ENB, Vless ); //neste caso e o maior pois o erro e negativo
+
+
 
   digitalWrite(motor1_a , LOW);
+
   digitalWrite(motor1_b , LOW);
+
   digitalWrite(motor2_a , LOW);
+
   digitalWrite(motor2_b , HIGH);
-  // delay(2000);
+
+  Serial.print("giro a direita");
+  Serial.print("\t");
+  Serial.print("motor ENB");
+  Serial.print("\t");
+  Serial.print(Vless);
+  Serial.print("\t");
+  Serial.print("motor ENA");
+  Serial.print("\t");
+  Serial.print(Vplus);
+
 }
 
-void motores_parados()
+void curva_direita(int velocidade, int respPI)
 {
+
+  Vplus = velocidade + respPI;
+  if (Vplus > 255) {
+    Vplus = 255;
+  } else if (Vplus < 0) {
+    Vplus = 0;
+  }
+
+  Vless = velocidade - respPI;
+  if (Vless > 255) {
+    Vless = 255;
+  } else if (Vless < 0) {
+    Vless = 0;
+
+  }
+
+  analogWrite(ENA, Vplus ); //neste caso e o menor pois o erro e negativo
+
+  analogWrite(ENB, Vless ); //neste caso e o maior pois o erro e negativo
+
+
   digitalWrite(motor1_a , HIGH);
-  digitalWrite(motor1_b , HIGH);
-  digitalWrite(motor2_a , HIGH);
+
+  digitalWrite(motor1_b , LOW);
+
+  digitalWrite(motor2_a , LOW);
+
   digitalWrite(motor2_b , HIGH);
-  //  delay(1000);
+
+  Serial.print("curva a direita");
+  Serial.print("\t");
+  Serial.print("motor ENB");
+  Serial.print("\t");
+  Serial.print(Vless);
+  Serial.print("\t");
+  Serial.print("motor ENA");
+  Serial.print("\t");
+  Serial.print(Vplus);
+}
+
+
+void motores_parados()
+
+{
+
+
+  digitalWrite(motor1_a , HIGH);
+
+  digitalWrite(motor1_b , HIGH);
+
+  digitalWrite(motor2_a , HIGH);
+
+  digitalWrite(motor2_b , HIGH);
+  Serial.print("\t");
+  Serial.print("desliguei");
+  delay(5000);
+
 }
 
 void setup() {
+
   Serial.begin(9600);
   pinMode(13, OUTPUT);
-  mpu6050_init();
+
+
 }
 
+
+
 void loop() {
+
+
+  velocidade_f = 255;
+  velocidade_1 = 255;
+  velocidade_2 = 200;
+  velocidade_3 = 140;
+  velocidade_4 = 80;
+  velocidade_5 = 40;
+
+  leitura0 = analogRead(sensor_0);
+
   leitura1 = analogRead(sensor_1);
+
   leitura2 = analogRead(sensor_2);
+
   leitura3 = analogRead(sensor_3);
 
-  //teste sensores//
-  #ifdef TESTE_SENSORES
-  Serial.print(leitura1);
-  Serial.print("\t");
-  Serial.print(leitura2);
-  Serial.print("\t");
-  Serial.println(leitura3);
-  Serial.print("\t");
-  Serial.println();
-  #endif
+  leitura4 = analogRead(sensor_4);
 
-  #ifdef PRINCIPAL
+
+  while ( robo_ligado == 0) {
+    delay (1000);
+    robo_ligado++;
+  }
+
+  //teste sensores//
+
+#ifdef TESTE_SENSORES
+
+  Serial.print(leitura0);
+
+  Serial.print("\t");
+
+  Serial.print(leitura1);
+
+  Serial.print("\t");
+
+  Serial.print(leitura2);
+
+  Serial.print("\t");
+
+  Serial.print(leitura3);
+
+  Serial.print("\t");
+
+  Serial.print(leitura4);
+
+  Serial.print("\t");
+
+  Serial.println();
+#endif
+
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifdef PRINCIPAL
+  if (millis() >= 13250  && millis() < 15750) { //USAR UM SEGUNDO A MENOS QUE O DESEJADO
+    Kp = 15;
+    Kd = 35;
+    velocidade_f = 33;
+    velocidade_1 = 28;
+    velocidade_2 = 25;
+    velocidade_3 = 25;
+    velocidade_4 = 25;
+    velocidade_5 = 25;
+    digitalWrite(13, HIGH);
+  } else {
+    Kp = CP;
+    Kd = CPD;
+  velocidade_f = 255;
+  velocidade_1 = 240;
+  velocidade_2 = 200;
+  velocidade_3 = 130;
+  velocidade_4 = 80;
+  velocidade_5 = 40;
+    digitalWrite(13, LOW);
+  }
   //Bloco de montagem das estrutura do array_sensores//
-  if (leitura1 > LIMIAR_SENSORES && leitura2 > LIMIAR_SENSORES && leitura3 > LIMIAR_SENSORES)
+  //if ((leitura0 < LIMIAR_SENSORES && leitura1 < LIMIAR_SENSORES && leitura2 < LIMIAR_SENSORES && leitura3 < LIMIAR_SENSORES && leitura4 < LIMIAR_SENSORES) && millis() >= 22000)
+  if (millis() >= 19200)
   {
-    array_sensores = 0b111;
-  } else if (leitura1 < LIMIAR_SENSORES && leitura2 > LIMIAR_SENSORES && leitura3 < LIMIAR_SENSORES)
+    /* linha_final = millis();
+      while(1){
+
+       if((millis() - linha_final) > 9000)
+    */
+    frente(velocidade_f);
+    digitalWrite(13, HIGH);
+
+    {
+      motores_parados();
+
+    }
+
+
+  } else if (leitura0 > LIMIAR_SENSORES && leitura1 > LIMIAR_SENSORES && leitura2 < LIMIAR_SENSORES && leitura3 > LIMIAR_SENSORES && leitura4 > LIMIAR_SENSORES)
   {
-    array_sensores = 0b010;
-  } else if (leitura1 < LIMIAR_SENSORES && leitura2 < LIMIAR_SENSORES && leitura3 > LIMIAR_SENSORES)
+
+    array_sensores = 0b00100;
+
+  } else if (leitura0 > LIMIAR_SENSORES && leitura1 > LIMIAR_SENSORES && leitura2 > LIMIAR_SENSORES && leitura3 < LIMIAR_SENSORES && leitura4 > LIMIAR_SENSORES)
   {
-    array_sensores = 0b001;
-  } else if (leitura1 < LIMIAR_SENSORES && leitura2 > LIMIAR_SENSORES && leitura3 < LIMIAR_SENSORES)
+
+    array_sensores = 0b00010;
+
+  } else if (leitura0 > LIMIAR_SENSORES && leitura1 > LIMIAR_SENSORES && leitura2 < LIMIAR_SENSORES && leitura3 < LIMIAR_SENSORES && leitura4 > LIMIAR_SENSORES)
   {
-    array_sensores = 0b010;
-  } else if (leitura1 < LIMIAR_SENSORES && leitura2 > LIMIAR_SENSORES && leitura3 > LIMIAR_SENSORES)
+
+    array_sensores = 0b00110;
+
+  } else if (leitura0 > LIMIAR_SENSORES && leitura1 < LIMIAR_SENSORES && leitura2 > LIMIAR_SENSORES && leitura3 > LIMIAR_SENSORES && leitura4 > LIMIAR_SENSORES)
   {
-    array_sensores = 0b011;
-  } else if (leitura1 > LIMIAR_SENSORES && leitura2 < LIMIAR_SENSORES && leitura3 < LIMIAR_SENSORES)
+
+    array_sensores = 0b01000;
+
+  } else if (leitura0 > LIMIAR_SENSORES && leitura1 < LIMIAR_SENSORES && leitura2 < LIMIAR_SENSORES && leitura3 > LIMIAR_SENSORES && leitura4 > LIMIAR_SENSORES)
   {
-    array_sensores = 0b100;
-  } else if (leitura1 > LIMIAR_SENSORES && leitura2 > LIMIAR_SENSORES && leitura3 < LIMIAR_SENSORES)
+
+    array_sensores = 0b01100;
+
+  } else if (leitura0 < LIMIAR_SENSORES && leitura1 < LIMIAR_SENSORES && leitura2 > LIMIAR_SENSORES && leitura3 > LIMIAR_SENSORES && leitura4 > LIMIAR_SENSORES)
   {
-    array_sensores = 0b110;
+
+    array_sensores = 0b11000;
+
+  } else if (leitura0 < LIMIAR_SENSORES && leitura1 < LIMIAR_SENSORES && leitura2 < LIMIAR_SENSORES && leitura3 > LIMIAR_SENSORES && leitura4 > LIMIAR_SENSORES)
+  {
+
+    array_sensores = 0b11100;
+
+  } else if (leitura0 < LIMIAR_SENSORES && leitura1 > LIMIAR_SENSORES && leitura2 > LIMIAR_SENSORES && leitura3 > LIMIAR_SENSORES && leitura4 > LIMIAR_SENSORES)
+  {
+
+    array_sensores = 0b10000;
+
+  } else if (leitura0 > LIMIAR_SENSORES && leitura1 > LIMIAR_SENSORES && leitura2 < LIMIAR_SENSORES && leitura3 < LIMIAR_SENSORES && leitura4 < LIMIAR_SENSORES)
+  {
+
+    array_sensores = 0b00111;
+
+  } else if (leitura0 > LIMIAR_SENSORES && leitura1 > LIMIAR_SENSORES && leitura2 > LIMIAR_SENSORES && leitura3 < LIMIAR_SENSORES && leitura4 < LIMIAR_SENSORES)
+  {
+
+    array_sensores = 0b00011;
+
+  } else if (leitura0 > LIMIAR_SENSORES && leitura1 > LIMIAR_SENSORES && leitura2 > LIMIAR_SENSORES && leitura3 > LIMIAR_SENSORES && leitura4 < LIMIAR_SENSORES)
+  {
+
+    array_sensores = 0b00001;
+
+  } else if (leitura0 < LIMIAR_SENSORES && leitura1 < LIMIAR_SENSORES && leitura2 < LIMIAR_SENSORES && leitura3 < LIMIAR_SENSORES && leitura4 > LIMIAR_SENSORES)
+  {
+
+    array_sensores = 0b11110;
+
+  } else if (leitura0 > LIMIAR_SENSORES && leitura1 < LIMIAR_SENSORES && leitura2 < LIMIAR_SENSORES && leitura3 < LIMIAR_SENSORES && leitura4 < LIMIAR_SENSORES)
+  {
+
+    array_sensores = 0b01111;
+
   }
 
   //Calculo dos erros//
-  if (array_sensores == 0b100) {
+
+  if (array_sensores == 0b01000) {
+
     erro_array = 2;
 
-  } else if (array_sensores == 0b010) {
+  } else if (array_sensores == 0b00100) {
+
     erro_array = 0;
 
-  } else if (array_sensores == 0b001) {
+
+  } else if (array_sensores == 0b11110) {
+
+    erro_array = 6;
+
+
+  } else if (array_sensores == 0b00010) {
+
     erro_array = -2;
 
-  } else if (array_sensores == 0b110) {
-    erro_array = 1;
+  } else if (array_sensores == 0b00011 ) {
 
-  } else if (array_sensores == 0b011) {
+    erro_array = -3;
+
+
+  } else if (array_sensores == 0b00110) {
+
     erro_array = -1;
 
-  } else if (array_sensores == 0b111) {
+
+  } else if (array_sensores == 0b01100) {
+
+    erro_array = 1;
+
+
+  } else if (array_sensores == 0b10000) {
+
+    erro_array = 4;
+
+
+  } else if (array_sensores == 0b11000) {
+
     erro_array = 3;
 
-  } else if (array_sensores == 0b000) {
-    erro_array = 0;
+
+  } else if (array_sensores == 0b00001) {
+
+    erro_array = -4;
+
+
+  } else if (array_sensores == 0b11100) {
+
+    erro_array = 5;
+
+
+  } else if (array_sensores == 0b00111) {
+
+    erro_array = -5;
+
 
   }
-  #ifdef MOSTRAR_VALORES
+
+
+
+
+#ifdef MOSTRAR_VALORES
+
   Serial.print("Valor do erro:");
+
   Serial.print("  ");
+
   Serial.print(erro_array);
+
   Serial.print("  ");
-  #endif
+
+#endif
+
   //Compensador PID//
+
   erro_atual = erro_array;
+
   resposta_p = erro_atual * Kp;
-  resposta_i = resposta_i + erro_atual * Ki * A;
+
   resposta_d = Kd * (erro_atual - erro_anterior);
-  Resposta_PID = resposta_p + resposta_i + resposta_d;
+
+  Resposta_PID = resposta_p + resposta_d;
+
   erro_anterior = erro_atual;
-  #ifdef MOSTRAR_VALORES
+
+#ifdef MOSTRAR_VALORES
+
   Serial.print("\t");
+
   Serial.print("Resposta PID:");
+
   Serial.print("  ");
+
   Serial.print(Resposta_PID);
+
   Serial.print("  ");
-  #endif
+
+#endif
+
+
 
   //PRINCIPAL//
-  if (erro_array == 0) {
-    frente(velocidade);
-  }
-  if (erro_array == 1) {
-    giro_a_esquerda(velocidade, Resposta_PID); // erro positivo virar à esqerda
-  }
-  if (erro_array == 2) {
-    giro_a_esquerda(velocidade, Resposta_PID);
-  }
-  if (erro_array == -1) {
-    giro_a_direita(velocidade, Resposta_PID); // erro negativo virar à esquerda
-  }
-  if (erro_array == -2) {
-    giro_a_direita(velocidade, Resposta_PID);
-  }
-  if (erro_array == 3) {
-    motores_parados();
-    //delay(5000);
-  }
-  #ifdef MOSTRAR_VALORES
-  Serial.print("\t");
-  Serial.print("V+:");
-  Serial.print("  ");
-  Serial.print(Vplus);
-  Serial.print("  ");
-  Serial.print("\t");
-  Serial.print("V-:");
-  Serial.print("  ");
-  Serial.println(Vless);
-  #endif
 
-  #endif
+  if (erro_array == 0) {
+
+    frente(velocidade_f);
+
+  }
+
+  if (erro_array == 1) {
+
+    //giro_a_esquerda(velocidade, Resposta_PID); // erro positivo virar à esqerda
+    curva_esquerda(velocidade_1, Resposta_PID);
+  }
+
+  if (erro_array == 2) {
+
+    //giro_a_esquerda(velocidade, Resposta_PID);
+    curva_esquerda(velocidade_2, Resposta_PID);
+
+  } if (erro_array == 3) {
+
+    //giro_a_esquerda(velocidade, Resposta_PID);
+    curva_esquerda(velocidade_3, Resposta_PID);
+
+  } if (erro_array == 4) {
+
+    //giro_a_esquerda(velocidade, Resposta_PID);
+    curva_esquerda(velocidade_4, Resposta_PID);
+
+  } if (erro_array == 5) {
+
+    giro_a_esquerda(velocidade_5, Resposta_PID);
+    //curva_esquerda(velocidade, Resposta_PID);
+
+  } if (erro_array == 6) {
+
+    giro_a_esquerda(velocidade_5, Resposta_PID);
+    //curva_esquerda(velocidade, Resposta_PID);
+
+  }
+
+  if (erro_array == -1) {
+
+    //giro_a_direita(velocidade, Resposta_PID); // erro negativo virar à esquerda
+    curva_direita(velocidade_1, Resposta_PID);
+  }
+
+  if (erro_array == -2) {
+
+    //giro_a_direita(velocidade, Resposta_PID);
+    curva_direita(velocidade_2, Resposta_PID);
+
+  } if (erro_array == -3) {
+
+    //giro_a_direita(velocidade, Resposta_PID);
+    curva_direita(velocidade_3, Resposta_PID);
+
+  } if (erro_array == -4) {
+
+    //giro_a_direita(velocidade, Resposta_PID);
+    curva_direita(velocidade_4, Resposta_PID);
+
+  } if (erro_array == -5) {
+
+    giro_a_direita(velocidade_5, Resposta_PID);
+    //curva_direita(velocidade, Resposta_PID);
+
+  }
+
+
+
+#ifdef MOSTRAR_VALORES
+
+  Serial.print("\t");
+
+  Serial.print("V+:");
+
+  Serial.print("  ");
+
+  Serial.print(Vplus);
+
+  Serial.print("  ");
+
+  Serial.print("\t");
+
+  Serial.print("V-:");
+
+  Serial.print("  ");
+
+  Serial.println(Vless);
+
+#endif
+
+
+
+#endif
+
 
 }
